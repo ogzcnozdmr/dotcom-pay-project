9fdad86f940a35a6441cf1c22a74e13a28c8f72e
Oğuzcan Özdemir <o.ozdmr.40@gmail.com>
2023-04-14 10:59:35 +0300

    updates

--- resources/views/general/footer.blade.php
+++ resources/views/general/footer.blade.php
@@ -2,5 +2,5 @@
 <footer class="footer">
     © 2023 <span class="d-none d-sm-inline-block"> - Created by <i class="mdi mdi-heart text-danger"></i> Dotcomsoft</span>.
     <br>
-    Kuante Duş ve Kabin, Ereğli Kapı Mahallesi 10, Acem Cd. No:55/A, 68000 Aksaray (0382) 212 11 58
+    Kuante Duş ve Kabin firması için hazırlanmıştır <br> Ereğli Kapı Mahallesi 10, Acem Cd. No:55/A, 68000 Aksaray (0382) 212 11 58
 </footer>
